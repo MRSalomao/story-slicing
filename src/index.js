2c58ff5dccc50bd1c9aa73630a9c7abdd606709a
Marcello Salomao <marcello.eci@gmail.com>
2021-07-13 10:48:34 +1000

    slice 3: hardcoded state/tax

--- src/index.js
+++ src/index.js
@@ -5,9 +5,27 @@ document.getElementById('app').innerHTML = `
 <div>
   Units: 100
   Price: $20
+  State: CA
 
-  Total: $2000
+  Total: <span id="total"></span>
+
+  Active tax rate: <span id="tax-rate"></span>
 </div>
 `;
+
+const totalRef = document.getElementById('total');
+const taxRateRef = document.getElementById('tax-rate');
+
+const taxes = {
+  UT: 6.85,
+  NV: 8.0,
+  TX: 6.25,
+  AL: 4.0,
+  CA: 8.25
+};
+
+totalRef.innerHTML = (100 * 20 * (100 - taxes['CA'])) / 100;
+
+taxRateRef.innerHTML = taxes['CA'] + '%';
 
 // 1000, 3 ; 5000, 5; 7, 7000, 10000, 10; 50000, 15 / UT, 6.85; NV, 8.00
