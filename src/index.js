f61615b68e26465319cd6053d280bd20f8278802
Marcello Salomao <marcello.eci@gmail.com>
2021-07-13 10:36:39 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+// 1000, 3 ; 5000, 5; 7, 7000, 10000, 10; 50000, 15 / UT, 6.85; NV, 8.00
